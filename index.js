c30ce1525727d5b0d6af549228f1db46a5caa8d6
hoangtranson <son.hoang01@gmail.com>
2024-09-22 19:58:02 +0700

    feat: 🎸 add relationship for genre and courses

--- index.js
+++ index.js
@@ -7,18 +7,55 @@ const GENRES = [
 
 const COURSES = [
   {
-    id: "1",
+    id: "book-01",
     name: "JavaScript",
     description: "JavaScript course",
     price: 100,
     discount: true,
+    genreId: "cat-01",
   },
   {
-    id: "2",
+    id: "book-02",
     name: "React",
     description: "React course",
     price: 200,
     discount: false,
+    genreId: "cat-02",
+  },
+  {
+    id: "book-08",
+    name: "NextJS Basics",
+    description: "NextJS Basics for beginners",
+    price: 599.99,
+    discount: false,
+    genreId: "cat-01",
+  },
+
+  {
+    id: "book-21",
+    name: "The Immortals of Meluha",
+    description: "Shiva Trilogy -1",
+    price: 299.99,
+    discount: false,
+    genreId: "cat-02",
+  },
+
+  {
+    id: "book-22",
+    name: "The Secret of the Nagas",
+    description: "Shiva Trilogy -2",
+    price: 199.99,
+    discount: true,
+    genreId: "cat-02",
+  },
+
+  {
+    id: "book-23",
+    name: "The Oath of the Vayuputras",
+    description: "Shiva Trilogy -3",
+    price: 599.99,
+    discount: false,
+    genreId: "cat-02",
   },
 ];
 
@@ -40,6 +77,7 @@ const typeDefs = gql`
     description: String!
     price: Float!
     discount: Boolean!
+    genre: Genre
   }
 
   type Genre {
@@ -47,7 +85,6 @@ const typeDefs = gql`
     name: String!
   }
 `;
-
 
 const resolvers = {
   Query: {
@@ -64,6 +101,11 @@ const resolvers = {
     genres: () => GENRES,
     genre: (parent, args, context, info) => {
       return GENRES.find((genre) => genre.id === args.id);
+    },
+  },
+  Course: {
+    genre: (parent, args, context, info) => {
+      return GENRES.find((genre) => genre.id === parent.genreId);
     },
   },
 };
